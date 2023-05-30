27430bd824050e1072851d49842fd801575979f3
Aiperiwa <periwa003@gmail.com>
2023-05-30 14:42:05 +0600

    name

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import Img from "../../image/headerImg.png"
+import Name from "../../image/Black_Minimalist_Simple_Personal_Name_Logo-removebg-preview.png"
+import Style from "./header.module.css"
 
 const Header = () => {
  return (
   <div>
-    <img src={Img} alt="" />
+    <img className={Style.pic} src={Img} alt="" /> 
+    <img className={Style.name} src={Name} alt="" />
 
   </div>
  )
